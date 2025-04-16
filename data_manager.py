1afdb69b9c56863df3f152e808139905b82ac9fb
gerriv-dev <gerriv.dev@web.de>
2025-04-16 16:45:29 +0200

    Überprüfung der Konfigurationsdatei eingebaut

--- data_manager.py
+++ data_manager.py
@@ -1,21 +1,41 @@
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from json import loads
+import sys
 
 
 def get_data():
-    file = filedialog.askopenfile(
-        title="Wähle eine kompatible Konfigurationsdatei . . .",
-        defaultextension=".json",
-        filetypes=[("Konfigurationsdateien", "*.memory")],
-        initialdir=".",
-    )
-    content = loads(file.read())
-    name = content["name"]
-    cols = content["cols"]
-    lines = content["lines"]
-    front_bg = content["front-background-color"]
-    back_bg = content["back-background-color"]
-    color = content["color"]
-    accent = content["accent-color"]
-    data = content["data"]
-    return name, cols, lines, front_bg, back_bg, color, accent, data
+    while True:
+        file = filedialog.askopenfile(
+            title="Wähle eine kompatible Konfigurationsdatei . . .",
+            defaultextension=".json",
+            filetypes=[("Konfigurationsdateien", "*.memory")],
+            initialdir=".",
+        )
+
+        if file is None:
+            sys.exit()
+
+        try:
+            content = loads(file.read())
+            name = content["name"]
+            cols = content["cols"]
+            lines = content["lines"]
+            front_bg = content["front-background-color"]
+            back_bg = content["back-background-color"]
+            color = content["color"]
+            accent = content["accent-color"]
+            data = content["data"]
+
+            if len(data) <= cols * lines:
+                return name, cols, lines, front_bg, back_bg, color, accent, data
+            else:
+                messagebox.showwarning(
+                    "Achtung",
+                    "Die Zahl der Zeilen und Spalten stimmt nicht mit der Anzahl der Datensätze überein.",
+                )
+
+        except AttributeError:
+            messagebox.showerror(
+                "Fehler",
+                "Die geöffnete Datei ist leider nicht kompatibel. Bitte öffnen Sie eine kompatible Datei.",
+            )
